6afe8339964e09c08a8aed1e526fb3032ba8e6a4
kommissar_in_yellow <andre.m.perkins@gmail.com>
2026-01-26 00:28:03 -0700

    [vt0-smoke-tests-001] Inside fire/work, create two files: calc.py and test_calc.py. In calc.py, write a small function add(a, b) that is intentionally wrong in a simple way. In test_calc.py, write a few small tests using Python’s built-in unittest that should fail because of that bug. Also create a file fire/work/README.txt that explains how to run the tests with: python -m unittest. Do not modify anything outside the fire folder.

---
+++ fire/work/test_calc.py
@@ -0,0 +1,7 @@
+import unittest
+from calc import add
+class TestAdd(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+if __name__ == '__main__':
+    unittest.main()
